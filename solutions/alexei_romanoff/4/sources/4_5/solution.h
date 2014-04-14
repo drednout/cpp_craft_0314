255811f5c2179eb8329c4fdd4ab6ff4270a46e73
Alexei Romanoff <drednout.by@gmail.com>
2014-04-14 23:51:51 +0300

    Multi-threaded min/max calculation has been implemented

--- solutions/alexei_romanoff/4/sources/4_5/solution.h
+++ solutions/alexei_romanoff/4/sources/4_5/solution.h
@@ -3,9 +3,17 @@
 
 #include <vector>
 
+#include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
+
 namespace task4_5
 {
+    class solution;
+
 	typedef std::vector< std::vector< int > > data_type;
+    typedef std::vector< int > data_container;
+    typedef std::list< boost::thread* > thread_list;
+    typedef void (solution::* data_function)(const data_container &data) const;
 
 	class solution
 	{
@@ -13,7 +21,22 @@ namespace task4_5
 		explicit solution( const data_type& data );
 		int get_min() const;
 		int get_max() const;
+    private:
+        unsigned int thread_count_;
+
+        mutable int min_value_;
+        mutable boost::mutex min_lock_;
+
+        mutable int max_value_;
+        mutable boost::mutex max_lock_;
+
+        data_type data_;
+
+        void calc_min(const data_container &data) const;
+        void calc_max(const data_container &data) const;
+        void do_thread_calculation(const data_function f) const;
 	};
+
 }
 
 #endif // _TASK4_5_SOLUTION_H_
